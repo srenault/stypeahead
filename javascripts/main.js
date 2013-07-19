9e347d46bc4978d1fed6e8c486c035d8cdd8e552
Sebastien Renault <sre@zenexity.com>
2013-07-19 11:14:37 +0200

    Initial commit

---
+++ javascripts/main.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+
+    /*
+     * Classic boostrap typeahead.
+     */
+    $('.input-multiple .hidden-autocomplete').typeahead({
+        menu: '<ul class="dropdown-menu select-tag"></ul>',
+        source: ["butter", "sugar", "lemon"],
+        updater: function(tag) {
+            var $inputMultiple = this.$element.closest('.input-multiple'),
+                $el = $inputMultiple.find('.autocomplete');
+            $('<li class="added" data-item="'+ tag +'">' + tag + '</li>').insertBefore($el.parent());
+            $inputMultiple.append('<input type="hidden" name="' + $el.attr('name') + '" value="'+ tag +'"/>');
+            $el.val('');
+            return '';
+        },
+        highlighter: function(v) { return v; },
+        minLength: 0
+    });
+
+    /*
+     * Close suggestions on clickout.
+     */
+    $('html').on('click', function(e) {
+        $('.input-multiple button.select').removeClass('open');
+        $('.dropdown-menu').hide();
+    });
+
+    /*
+     * Open select with full list.
+     */
+    $('.input-multiple button.select').on('click', function(e) {
+        var $button = $(this),
+            $inputMultiple = $button.closest('.input-multiple');
+
+        if(!$button.is('.open')) {
+            window.setTimeout(function() {
+                var $input = $inputMultiple.find('.hidden-autocomplete');
+                $input.focus();
+                $input.data('backup', $input.val());
+                $input.val('').data('show-all', true);
+                $input.typeahead('lookup');
+                $input.val($input.data('backup')).removeData('show-all');
+                $button.addClass('open');
+            }, 100);
+        } else {
+            $button.removeClass('open');
+        }
+    });
+
+    /*
+     * Give text focus.
+     */
+    $('.input-multiple > ul').on('click', function() {
+        $(this).find('.autocomplete').focus();
+    });
+
+    /*
+     * Proxify keydown key.
+     */
+    $('.input-multiple .autocomplete').on('keydown', function(e) {
+        var $autocomplete = $(this).closest('.input-multiple').find('.hidden-autocomplete');
+        $autocomplete.trigger(e);
+        if(e.keyCode == 13) e.preventDefault();
+    });
+
+    /*
+     * Proxify keyup key.
+     */
+    $('.input-multiple .autocomplete').on('keyup', function(e) {
+        var $input = $(this),
+            $inputMultiple = $input.closest('.input-multiple'),
+            $autocomplete = $inputMultiple.find('.hidden-autocomplete');
+
+        $autocomplete.val($input.val());
+        $autocomplete.trigger(e);
+
+        var nbCar = $input.val().length;
+        $input.attr("size",  nbCar ? nbCar * 1.5 : 1);
+    });
+
+    /*
+     * Delete items with keyboard.
+     */
+    $('.input-multiple .autocomplete').on('keydown', function(e) {
+        var $input = $(this),
+            $item = $input.parent().prev('li'),
+            $inputMultiple = $input.closest('.input-multiple');
+
+        if(e.keyCode == 8 && !$input.val()) { //RETURN
+            if($input.is('.confirm-delete')) {
+                $input.removeClass('confirm-delete');
+                $item.remove();
+                $inputMultiple.find('input[value=' + $item.data('item') + ']').remove();
+            } else  {
+                $(this).addClass('confirm-delete');
+            }
+        } else {
+            $input.removeClass('confirm-delete');
+        }
+    });
+
+    /*
+     * Remove the item.
+     */
+    $('body').on('click', '.input-multiple li:not(.new-item)', function(e) {
+        var $item = $(this);
+        $item.closest('.input').find('input[value=' + $item.data('item') + ']').remove();
+        $item.remove();
+    });
+});
